9e6efd18802765e7d7987812b6d4153c9c2ebab5
Lucas Iglesias <lucasiglesias31@gmail.com>
2022-11-04 00:37:05 -0300

    se modifico el archivo numero.php

--- numero_magico.py
+++ numero_magico.py
@@ -2,5 +2,6 @@ import random
 number = 0
 for _ in range(0,10):
 	number += random.randint(1,10)
-number = int(number/9)
+number = int(number/10)
 print(f"Tu numero magico es: {number}")
+programando en casa con el titi
