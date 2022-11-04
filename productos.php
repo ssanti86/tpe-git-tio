4bd245d9c4e1af7774cb26d31f81863bae3770e6
Ignacio Maile <nachomaile10@gmail.com>
2022-11-04 00:48:31 -0300

    se modifico el archivo produtos.php

--- productos.php
+++ productos.php
@@ -3,6 +3,7 @@
 $productos = ["teclado","mouse","monitor","auriculares","escritorio", "procesador", "gabinete", "mousepad"];
 
 foreach ($productos as $producto) {
+	echo "Producto:";
 	echo $producto;
 }
 
