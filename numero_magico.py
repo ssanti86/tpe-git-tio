6713202528ae7532b3f782d731c662923eefdca2
Ignacio Maile <nachomaile10@gmail.com>
2022-11-04 01:14:31 -0300

    se modifico de nuevo el archivo .py

--- numero_magico.py
+++ numero_magico.py
@@ -4,4 +4,4 @@ for _ in range(0,10):
 	number += random.randint(1,10)
 number = int(number/10)
 print(f"Tu numero magico es: {number}")
-programando en casa con el titi
+aprendiendo a programar con el titi y el ñago!
